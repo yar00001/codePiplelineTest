714a060ba3d417637bd244dc4a66b66d5a5f1dd3
Hugh Yar <yarhooma@amazon.com>
2018-01-16 18:20:26 -0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Sample Node.js application
+ */
+
+var time = require('time');
+exports.handler = (event, context, callback) => {
+    var currentTime = new time.Date(); 
+    currentTime.setTimezone("America/Los_Angeles");
+    callback(null, {
+        statusCode: '200',
+        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+    });
+};
